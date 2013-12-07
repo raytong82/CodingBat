e18b041b5eb03d59456094ec9708b7a4b5691069
Raymond Tong <raytong82@gmail.com>
2013-12-07 20:47:47 +0800

    remove deprecated matcher and use mix-in

--- src/test/scala/com/raytong/warmup1/QuestionTestSuite.scala
+++ src/test/scala/com/raytong/warmup1/QuestionTestSuite.scala
@@ -3,12 +3,12 @@ package com.raytong.warmup1
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.matchers.ShouldMatchers._
+import org.scalatest.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks._
 import com.raytong.warmup1.Question._
 
 @RunWith(classOf[JUnitRunner])
-class QuestionTestSuite extends FunSuite {
+class QuestionTestSuite extends FunSuite with Matchers {
 
   val sleepInData = Table(
     ("weekday", "vacation", "result"),
@@ -20,7 +20,7 @@ class QuestionTestSuite extends FunSuite {
   test("sleepIn") {
     forAll(sleepInData) {
       (weekday: Boolean, vacation: Boolean, result: Boolean) =>
-        sleepIn(weekday, vacation) should be === result
+        sleepIn(weekday, vacation) should be(result)
     }
   }
 
@@ -35,7 +35,7 @@ class QuestionTestSuite extends FunSuite {
   test("monkeyTrouble") {
     forAll(monkeyTroubleData) {
       (aSmile: Boolean, bSmile: Boolean, result: Boolean) =>
-        monkeyTrouble(aSmile, bSmile) should be === result
+        monkeyTrouble(aSmile, bSmile) should be(result)
     }
   }
 
@@ -54,7 +54,30 @@ class QuestionTestSuite extends FunSuite {
   test("sumDouble") {
     forAll(sumDoubleData) {
       (a: Int, b: Int, result: Int) =>
-        sumDouble(a, b) should be === result
+        sumDouble(a, b) should be(result)
+    }
+  }
+
+  val diff21Data = Table(
+    ("n", "result"),
+    (19, 2),
+    (10, 11),
+    (21, 0),
+    (22, 2),
+    (25, 8),
+    (30, 18),
+    (0, 21),
+    (1, 20),
+    (2, 19),
+    (-1, 22),
+    (-2, 23),
+    (50, 58)
+  )
+
+  test("diff21") {
+    forAll(diff21Data) {
+      (n: Int, result: Int) =>
+        diff21(n) should be(result)
     }
   }
 }
