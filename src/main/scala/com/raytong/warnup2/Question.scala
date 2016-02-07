16ea6b749fedd98546cd41a2cf922972d9946f22
raytong82 <raytong82@gmail.com>
2016-02-07 14:50:31 +0000

    array123

--- src/main/scala/com/raytong/warnup2/Question.scala
+++ src/main/scala/com/raytong/warnup2/Question.scala
@@ -72,7 +72,7 @@ object Question {
   }
 
   def array123(nums: List[Int]): Boolean = {
-    def findNum(nums: List[Int], target: Int): Boolean = {
+    def findNum(target: Int): Boolean = {
       nums.contains(target)
     }
     findNum(1) && findNum(2) && findNum(3)
